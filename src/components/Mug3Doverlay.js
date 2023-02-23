79ef2488277e35d281e8ab5daf251f8010694fc2
GUUS HOEBERECHTS <guus-99@live.nl>
2023-02-23 16:49:02 +0200

    mug collection working

---
+++ src/components/Mug3Doverlay.js
@@ -0,0 +1,40 @@
+import React, { Suspense, useState, useEffect } from 'react'
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls, useGLTF } from "@react-three/drei";
+
+
+const styles = {
+    height: "80vh",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%,-50%)",
+    // backgroundColor: "black"
+  }
+  
+  function Model({current}){
+
+  
+    const gltf = useGLTF(current);
+    return <primitive object ={gltf.scene} />
+  
+      }
+
+export default function Mug3Doverlay(props) {
+    
+
+    
+        return (
+          <>
+          <Suspense fallback={null}>
+          <Canvas camera={{ position: [0,0,-0.16]}} style={styles}>
+            <ambientLight intensity={1} />
+            <group>
+            <Model current={props.mug} />
+            </group>
+            <OrbitControls autoRotate enableZoom={false} />
+          </Canvas>
+          </Suspense>
+          </>
+        )
+}
--- src/pages/Collection.js
+++ src/pages/Collection.js
@@ -3,6 +3,7 @@ import '../index.css'
 import '../style/landing.css'
 import '../style/base.css'
 import Menu from '../components/Menu'
+import Mug3Doverlay from '../components/Mug3Doverlay'
 
 export default function Collection() {
   useEffect(() => {
@@ -21,6 +22,13 @@ export default function Collection() {
     setIcon(!icon)
   }
 
+  // const [shown, setShown] = useState(false);
+  const[currentMug, setCurrentMug] = useState()
+
+  // const handleOverlay = event => {
+  //   setShown(current => !current )
+  // }
+
   return (
     <>
     
@@ -30,17 +38,29 @@ export default function Collection() {
       <h1>First collection</h1>
 
       <div className="imageContainer">
-        <div><img src="img/mugs.png"/></div>
-        <div><img src="img/mugs.png"/></div>
-        <div><img src="img/mugs.png"/></div>
-        <div><img src="img/mugs.png"/></div>
-        <div><img src="img/mugs.png"/></div>
-        <div><img src="img/mugs.png"/></div>
-        <div><img src="img/mugs.png"/></div>
-        <div><img src="img/mugs.png"/></div>
-        <div><img src="img/mugs.png"/></div>
-        <div><img src="img/mugs.png"/></div>
+        <div><img src="img/mugpng/mugs1.png" onClick={() => setCurrentMug("firstmug.glb")}/></div>
+        <div><img src="img/mugpng/mugs2.png" onClick={() => setCurrentMug("poly2.glb")} /></div>
+        <div><img src="img/mugpng/mugs3.png"/></div>
+        <div><img src="img/mugpng/mugs4.png"/></div>
+        <div><img src="img/mugpng/mugs5.png"/></div>
+        <div><img src="img/mugpng/mugs6.png"/></div>
+        <div><img src="img/mugpng/mugs7.png"/></div>
+        <div><img src="img/mugpng/mugs8.png"/></div>
+        <div><img src="img/mugpng/mugs9.png"/></div>
+        <div><img src="img/mugpng/mugs10.png"/></div>
+        <div><img src="img/mugpng/mugs11.png"/></div>
+        <div><img src="img/mugpng/mugs12.png"/></div>
+        <div><img src="img/mugpng/mugs13.png"/></div>
+        <div><img src="img/mugpng/mugs14.png"/></div>
+        <div><img src="img/mugpng/mugs15.png"/></div>
+        <div><img src="img/mugpng/mugs16.png"/></div>
+        <div><img src="img/mugpng/mugs17.png"/></div>
+        <div><img src="img/mugpng/mugs18.png"/></div>
+        <div><img src="img/mugpng/mugs19.png"/></div>
+        <div><img src="img/mugpng/mugs20.png"/></div>
       </div>
+
+      {currentMug ? <div><Mug3Doverlay mug={currentMug} /> <div className="closeBtn" onClick={() => setCurrentMug()}><h4>close</h4></div></div> : ""}
 
     
     </div>
